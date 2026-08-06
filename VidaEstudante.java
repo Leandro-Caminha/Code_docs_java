cdad5384c3a780933e09672fbe5d91c9710088a5
Leandro_Caminha <leandrocaminharpereira@gmail.com>
2026-08-05 23:53:49 -0300

    Complete exercise 3: primitive variable

--- VidaEstudante.java
+++ VidaEstudante.java
@@ -3,10 +3,31 @@ import java.util.Scanner;
 public class VidaEstudante {
     public static void main(String[] args) {
         
-        Scanner npa = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("Digite sua nota da NPA: ");
-        int value = npa.nextInt();
+        System.out.print("Digite a nota da NP1: ");
+        float value1 = input.nextFloat();
+        System.out.print("Digite a nota da NP2: ");
+        float value2 = input.nextFloat();
 
+        float ret = value1 + value2;
+
+        if(ret < 30){
+            System.out.println("ALUNO REPROVADO");
+        }else if(ret >= 30 && ret < 60){
+            System.out.println("NP1 + NP2 = " + ret);
+            System.out.println("O ALUNO DEVE FAZER A NP3");
+            System.out.print("Digite a nota da NP3: ");
+            float value3 = input.nextFloat();
+            ret = (ret + value3) / 2;
+            if(ret < 50){
+                System.out.println("ALUNO REPROVADO");
+            }else{
+                System.out.println("ALUNO APROVADO");
+            }
+        }else{
+            System.out.println("ALUNO APROVADO");
+        }
+        input.close();
     }
 }
