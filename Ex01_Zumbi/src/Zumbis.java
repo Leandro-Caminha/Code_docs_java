f76a47d0822d500fcaef4adbb0092f33326cc584
Leandro_Caminha <leandro.pereira@intelbras.com.br>
2026-08-14 13:30:39 -0300

    OOP: Doing exercise 1

---
+++ Ex01_Zumbi/src/Zumbis.java
@@ -0,0 +1,20 @@
+public class Zumbis {
+
+    String name;
+    int age;
+    double height;
+    double weight;
+    int wickedness; // 1 - 10
+
+    public void whoop() {
+        System.out.println("Hahahahaha...");
+    }
+
+    public void run() {
+        System.out.println("Run...run...run...");
+    }
+
+    public void devour() {
+        System.out.println("Uhuhuhuhuh...");
+    }
+}
