2fb4ca2c693043bafbbbdee0fa51bbc0b26d45e2
Leandro_Caminha <leandrocaminharpereira@gmail.com>
2026-08-22 10:14:50 -0300

    OOP: Doing exercise 5

---
+++ Ex05_Jogo/src/Main.java
@@ -0,0 +1,12 @@
+/*
+Exercício 5 – Jogo!
+-Crie Classes em Java que atendam a seguinte especificação
+-Cada vez que o personagem toma dano ele perde 5 pontos de vida;
+-Cada vez que usa a arma, ela perde 2 pontos de resistência
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Ex05_Jogo/src/Personagem.java
@@ -0,0 +1,10 @@
+public class Personagem {
+
+    String nome;
+    int pontos;
+    Arma arma;
+
+    public usarArma(String personagemAlvo, int pontos) {
+
+    }
+}
