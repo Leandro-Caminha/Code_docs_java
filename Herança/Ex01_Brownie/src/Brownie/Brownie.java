ea32e4f28382aa851b16a6c6b73d39a713da7a23
Leandro Caminha <leandrocaminharpereira@gmail.com>
2026-09-16 22:38:20 -0300

    wip: Herança - Doing Ex01_Brownie

---
+++ Herança/Ex01_Brownie/src/Brownie/Brownie.java
@@ -0,0 +1,26 @@
+package Brownie;
+
+public class Brownie {
+
+    // Atributos
+    protected final String nome;
+    protected final double preco;
+    protected final String sabor;
+
+    // Construtor
+    public Brownie(String nome, double preco, String sabor) {
+        this.nome = nome;
+        this.preco = preco;
+        this.sabor = sabor;
+    }
+
+    // Métodos
+    public void addCarrinhoDeCompras() {
+    }
+
+    public void calculaValorTotalCompra() {
+    }
+
+    public void mostraInfo() {
+    }
+}
---
+++ Herança/Ex01_Brownie/src/Brownie/BrownieNutella.java
@@ -0,0 +1,5 @@
+package Brownie;
+
+public class BrownieNutella {
+
+}
